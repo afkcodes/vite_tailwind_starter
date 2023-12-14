fea2adfa3ddc9449c7d7f630157910ce1635164f
Ashish Kumar <ashish.kmr@outlook.com>
2023-12-14 11:44:58 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex justify-center items-center min-h-screen bg-black text-gray-400'>
+      <p>vite_tailwind_starter</p>
+    </div>
+  );
+}
+
+export default App;
